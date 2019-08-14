6e5010453a172cba3b50750917ed3980b7870835
TRT-PaulB <paul.bickell@raytheon.co.uk>
2019-08-14 09:58:39 +0100

    in progress

---
+++ react-counter-app/src/components/counters.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.counters.map(counter => (
+          <div key={counter.id}>
+            <Counter key={counter.id} value={counter.value} />
+          </div>
+        ))}
+      </React.Fragment>
+    );
+  } // selected={true}
+}
+
+export default Counters;
--- react-counter-app/src/components/mostPrevCounter.jsx
+++ react-counter-app/src/components/mostPrevCounter.jsx
@@ -5,6 +5,18 @@ class Counter extends Component {
     value: this.props.value
   };
 
+  //////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////
+  // could add child element within Counter in the Counters.jsx
+  // --> <h4>Counter {counter.id}</h4>
+  // whic means we could use: {this.props.children} above span to display them
+  // --> {this.props.children}
+
+  // render()
+  // console.log("props", this.props); // Plain javascript object, includes children
+  //////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////
+
   render() {
     return (
       <React.Fragment>
@@ -17,7 +29,7 @@ class Counter extends Component {
         </button>
       </React.Fragment>
     );
-  }
+  } // notice this.handleIncrement is an inline function because it is passing an argument
 
   // ARROW FUNCTIONS INHERIT THE THIS KEYWORD
   handleIncrement = () => {
