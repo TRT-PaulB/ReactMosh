7ab7d24aae1baef3c8487b6cceac4ef5afcd918b
TRT-PaulB <paul.bickell@raytheon.co.uk>
2019-08-15 12:59:01 +0100

    used as stateless functional component

--- react-vidly-counter/src/common/like.jsx
+++ react-vidly-counter/src/common/like.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-class Like extends Component {
-  render() {
-    const { liked, movie, onLikeOpinion, onClick } = this.props;
+// convert likeClassVersion.jsx into a stateless functional component
+// 1) create stateless functional component: sfc
+// 2) copy and paste content FROM after "render() {" of and before "}"
+//                           TO between "const Like = (props) => {"    and "}" (removing return statement)
+// 3) remember to supply props as an argument ('this' keyword cannot be used)      
+const Like = (props) => {
+  const { liked, movie, onLikeOpinion, onClick } = props;
   
     let iconVal = "heart";
     if (!liked) {
       iconVal += "-broken" 
     }
     
-    return <FontAwesomeIcon style={{cursor: "pointer"}}onClick={() => onLikeOpinion(movie)} icon={iconVal} />;
-
-    // ALTERNATIVE:  throw DOM event (rather than custom event above)   
-    //               return <FontAwesomeIcon onClick={onClick} icon={iconVal} />;
-  }
+    return <FontAwesomeIcon style={{cursor: "pointer"}} onClick={() => onLikeOpinion(movie)} icon={iconVal} />;
 }
-
+ 
 export default Like;
+
