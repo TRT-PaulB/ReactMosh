91276f3a62a3cb9b1c3f8262a562a01c8ea83129
TRT-PaulB <paul.bickell@raytheon.co.uk>
2019-08-15 13:55:46 +0100

    created new vidly extras ap with bostrap and font awesome

---
+++ react-vidly-extras/src/components/movies.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+import Like from "../common/like";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = movie => {
+    console.log("deleting here....");
+    const movies = this.state.movies.filter(mov => mov._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  handleLiked = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
+  render() {
+    const { length: moviesCount } = this.state.movies;
+
+    if (moviesCount === 0) return <p>No moves to display</p>;
+    return (
+      <React.Fragment>
+        <p>Showing {moviesCount} movies in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th>Liked</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like liked={movie.liked} onLikeOpinion={this.handleLiked} movie={movie}/>   
+                </td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;
+
+// ALTERNATIVE - receives the DOMN event from like.jsx and then rethrows
+// <Like liked={movie.liked} onClick={() => this.handleLiked(movie)} movie={movie}/>
+
+
+                   
+
+
+
--- react-vidly-extras/src/index.js
+++ react-vidly-extras/src/index.js
@@ -1,8 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
 
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -17,9 +18,7 @@ library.add(faHeartBroken)
 // see https://github.com/FortAwesome/react-fontawesome
 // FONT AWESOME [end]
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
