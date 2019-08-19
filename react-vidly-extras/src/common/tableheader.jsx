73b10aba521b1a3cc79071759cd911b93bf85b99
TRT-PaulB <paul.bickell@raytheon.co.uk>
2019-08-19 12:17:00 +0100

    creating new proecjt react-vidly-extras-rearrange

---
+++ react-vidly-extras/src/common/tableheader.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class TableHeader extends Component {
+  raiseSort = column => {
+    const sortColumn = { ...this.props.sortColumn };
+    if (sortColumn.column === column) {
+      // this is a direction change on the existing column sort
+      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+      sortColumn.column = column;
+    } else {
+      // this is a new column sort in ascending order
+      sortColumn.order = "asc";
+      sortColumn.column = column;
+    }
+
+    this.props.onSort(sortColumn); // THIS LINE PASSES BACK sortColumn via onSort function, relating to movies.handleSorting
+  };
+
+  render() {
+    return (
+      <thead>
+        <tr>
+          <th />
+        </tr>
+      </thead>
+    );
+  }
+}
+
+export default TableHeader;
