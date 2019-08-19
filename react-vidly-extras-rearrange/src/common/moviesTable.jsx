706bd4cc8118f4fbc7620d1b7488207b953819d4
TRT-PaulB <paul.bickell@raytheon.co.uk>
2019-08-19 16:21:58 +0100

    table abstraction completed

--- react-vidly-extras-rearrange/src/common/moviesTable.jsx
+++ react-vidly-extras-rearrange/src/common/moviesTable.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import Like from "../common/like";
-import TableHeader from "../common/tableheader";
-import TableBody from "../common/tableBody";
+import Like from "./like";
+import Table from "./table";
 
 // INPUT INTERFACE
 // columns: array
@@ -48,14 +47,12 @@ class MoviesTable extends Component {
     const { movies, onSort, sortColumn } = this.props;
 
     return (
-      <table className="table">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-        <TableBody data={movies} columns={this.columns} />
-      </table>
+      <Table
+        columns={this.columns}
+        data={movies}
+        onSort={onSort}
+        sortColumn={sortColumn}
+      />
     );
   }
 }
