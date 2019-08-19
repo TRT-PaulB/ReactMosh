71c3bf4a1a8c26e6b0c10ace575a850bdd8de901
TRT-PaulB <paul.bickell@raytheon.co.uk>
2019-08-19 11:57:54 +0100

    handle sorting within the table

--- react-vidly-extras/src/common/moviesTable.jsx
+++ react-vidly-extras/src/common/moviesTable.jsx
@@ -8,18 +8,33 @@ import Like from "../common/like";
 // 4) as there is no props argumnet automatically, use this.props
 
 class MoviesTable extends Component {
+  raiseSort = column => {
+    const sortColumn = { ...this.props.sortColumn };
+    if (sortColumn.column === column) {
+      // this is a direction change on the existing column sort
+      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+      sortColumn.column = column;
+    } else {
+      // this is a new column sort in ascending order
+      sortColumn.order = "asc";
+      sortColumn.column = column;
+    }
+
+    this.props.onSort(sortColumn); // THIS LINE PASSES BACK sortColumn via onSort function, relating to movies.handleSorting
+  };
+
   render() {
-    const { movies, onLike, onDelete, onSort } = this.props;
+    const { movies, onLike, onDelete } = this.props;
 
     return (
       <table className="table">
         <thead>
           <tr>
-            <th onClick={() => onSort("title")}>Title</th>
-            <th onClick={() => onSort("genre.name")}>Genre</th>
-            <th onClick={() => onSort("numberInStock")}>Stock</th>
-            <th onClick={() => onSort("dailyRentalRate")}>Rate</th>
-            <th onClick={() => onSort("liked")}>Liked</th>
+            <th onClick={() => this.raiseSort("title")}>Title</th>
+            <th onClick={() => this.raiseSort("genre.name")}>Genre</th>
+            <th onClick={() => this.raiseSort("numberInStock")}>Stock</th>
+            <th onClick={() => this.raiseSort("dailyRentalRate")}>Rate</th>
+            <th onClick={() => this.raiseSort("liked")}>Liked</th>
             <th />
           </tr>
         </thead>
--- react-vidly-extras/src/components/movies.jsx
+++ react-vidly-extras/src/components/movies.jsx
@@ -44,23 +44,22 @@ class Movies extends Component {
     this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
-  handleSorting = column => {
-    const sortColumn = { ...this.state.sortColumn };
-
-    if (sortColumn.column === column) {
-      // this is a direction change on the existing column sort
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-      sortColumn.column = column;
-    } else {
-      // this is a new column sort in ascending order
-      sortColumn.order = "asc";
-      sortColumn.column = column;
-    }
-
-    this.setState({
-      //sortColumn: { sortColumn } // not clear why this line would not work...
-      sortColumn: { column: sortColumn.column, order: sortColumn.order }
-    });
+  handleSorting = sortColumn => {
+    this.setState({ sortColumn });
+    // const sortColumn = { ...this.state.sortColumn };
+    // if (sortColumn.column === column) {
+    //   // this is a direction change on the existing column sort
+    //   sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+    //   sortColumn.column = column;
+    // } else {
+    //   // this is a new column sort in ascending order
+    //   sortColumn.order = "asc";
+    //   sortColumn.column = column;
+    // }
+    // this.setState({
+    //   //sortColumn: { sortColumn } // not clear why this line would not work...
+    //   sortColumn: { column: sortColumn.column, order: sortColumn.order }
+    // });
   };
 
   render() {
@@ -106,6 +105,7 @@ class Movies extends Component {
             onLike={this.handleLiked}
             onDelete={this.handleDelete}
             onSort={this.handleSorting}
+            sortColumn={sortColumn}
           />
           <Pagination
             currentPage={this.state.currentPage}
