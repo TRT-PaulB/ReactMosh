a1fbaedc15295f8e7318c3557431bbf1755578ea
TRT-PaulB <paul.bickell@raytheon.co.uk>
2019-08-19 11:37:48 +0100

    convert a sfc to a class

--- react-vidly-extras/src/common/moviesTable.jsx
+++ react-vidly-extras/src/common/moviesTable.jsx
@@ -1,49 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import Like from "../common/like";
 
-const MoviesTable = props => {
-  const { movies, onLike, onDelete, onSort } = props;
+// convert a sfc to a class:
+// 1) create sfc of the same name
+// 2) copy contents into reader content (omit inital return statement)
+// 3) remove 2nd export default
+// 4) as there is no props argumnet automatically, use this.props
 
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th onClick={() => onSort("title")}>Title</th>
-          <th onClick={() => onSort("genre.name")}>Genre</th>
-          <th onClick={() => onSort("numberInStock")}>Stock</th>
-          <th onClick={() => onSort("dailyRentalRate")}>Rate</th>
-          <th onClick={() => onSort("liked")}>Liked</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        {movies.map(movie => (
-          <tr key={movie._id}>
-            <td>{movie.title}</td>
-            <td>{movie.genre.name}</td>
-            <td>{movie.numberInStock}</td>
-            <td>{movie.dailyRentalRate}</td>
-            <td>
-              <Like
-                liked={movie.liked}
-                onLike={() => onLike(movie)}
-                movie={movie}
-              />
-            </td>
-            <td>
-              <button
-                onClick={() => onDelete(movie)}
-                className="btn btn-danger btn-sm"
-                movie={movie}
-              >
-                Delete
-              </button>
-            </td>
+class MoviesTable extends Component {
+  render() {
+    const { movies, onLike, onDelete, onSort } = this.props;
+
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th onClick={() => onSort("title")}>Title</th>
+            <th onClick={() => onSort("genre.name")}>Genre</th>
+            <th onClick={() => onSort("numberInStock")}>Stock</th>
+            <th onClick={() => onSort("dailyRentalRate")}>Rate</th>
+            <th onClick={() => onSort("liked")}>Liked</th>
+            <th />
           </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-};
+        </thead>
+        <tbody>
+          {movies.map(movie => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <Like
+                  liked={movie.liked}
+                  onLike={() => onLike(movie)}
+                  movie={movie}
+                />
+              </td>
+              <td>
+                <button
+                  onClick={() => onDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                  movie={movie}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
 
 export default MoviesTable;
