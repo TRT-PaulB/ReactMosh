1d01cad2d1fcb462573f013361e591287bbae104
TRT-PaulB <paul.bickell@raytheon.co.uk>
2019-08-21 13:56:09 +0100

    extracting input to reusable component

--- react-vidly-extras-forms/src/components/login.jsx
+++ react-vidly-extras-forms/src/components/login.jsx
@@ -1,30 +1,23 @@
 import React, { Component } from "react";
+import Input from "../common/input";
 
 class LoginForm extends Component {
   state = {
     // REACT: applying to state means we can be faithful to a single source of truth
-    account: { username: "", password: "" }
+    account: { username: "", password: "" } // control the elements, ie not null / undefined
   };
 
   handleSubmit = e => {
     e.preventDefault();
   };
 
-  //   handleChange = e => {
-  //     // check this in the react tab in chrome
-  //     const account = { ...this.state.account };
-  //     account.username = e.currentTarget.value;
-  //     this.setState({ account: account });
-  //   };
-
-  // CAN HANDLE MULTIPLE FIELDS (need a name field on the inputs)
   handleChange = ({ currentTarget: input }) => {
-    // ie destructuring from e.currentTarget   (renames to input)
-    // check this in the react tab in chrome
     const account = { ...this.state.account };
     account[input.name] = input.value;
     this.setState({ account: account });
   };
+
+  // const Input = ({ name, label, value, onChange }) => {
 
   render() {
     const { account } = this.state;
@@ -32,30 +25,19 @@ class LoginForm extends Component {
       <React.Fragment>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              autoFocus
-              id="username"
-              name="username"
-              type="text"
-              className="form-control"
-              value={account.username}
-              onChange={this.handleChange}
-            />
-          </div>
+          <Input
+            name="username"
+            label="Username"
+            value={account.username}
+            onChange={this.handleChange}
+          />
+          <Input
+            name="password"
+            label="Password"
+            value={account.password}
+            onChange={this.handleChange}
+          />
 
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              id="password"
-              name="password"
-              type="text"
-              className="form-control"
-              value={account.password}
-              onChange={this.handleChange}
-            />
-          </div>
           <button className="btn btn-primary">Login</button>
         </form>
       </React.Fragment>
