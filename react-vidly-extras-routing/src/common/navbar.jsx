be30f25865ccf4debcc8e7a18ea0b19ca279358c
TRT-PaulB <paul.bickell@raytheon.co.uk>
2019-08-21 09:46:57 +0100

    routing navbar working

--- react-vidly-extras-routing/src/common/navbar.jsx
+++ react-vidly-extras-routing/src/common/navbar.jsx
@@ -1,23 +1,46 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <ul>
-      <li>
-        <h2>Vidly</h2>
-      </li>
-      <li>
-        <NavLink to="/movies">Movies</NavLink>
-      </li>
-      <li>
-        <NavLink to="/customers">Customers</NavLink>
-      </li>
-      <li>
-        <NavLink to="/rentals">Rentals</NavLink>
-      </li>
-    </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Vidly
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/movies">
+              Movies
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/customers">
+              Customers
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/rentals">
+              Rentals
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
 export default NavBar;
+
+// https://getbootstrap.com/docs/4.3/components/navbar/
